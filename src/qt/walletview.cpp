b73fb1739b8b7ff2e0978eb6473ef17a93c87fa8
MotoAcidic <travisfinch01@gmail.com>
2018-11-25 11:40:16 -0500

    Update src/qt/walletview.cpp

--- src/qt/walletview.cpp
+++ src/qt/walletview.cpp
@@ -54,7 +54,7 @@ WalletView::WalletView(QWidget* parent) : QStackedWidget(parent),
     QPushButton* exportButton = new QPushButton(tr("&Export"), this);
     exportButton->setToolTip(tr("Export the data in the current tab to a file"));
 #ifndef Q_OS_MAC // Icons on push buttons are very uncommon on Mac
-    exportButton->setIcon(QIcon(GUIUtil::getThemeImage(":/icons/export")));
+    exportButton->setIcon(QIcon(":/icons/export"));
 #endif
     hbox_buttons->addStretch();
 
