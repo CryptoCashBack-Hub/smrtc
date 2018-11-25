4d631a8fb3aebdb2e00115db88e78ea3fe05cc6e
MotoAcidic <travisfinch01@gmail.com>
2018-11-25 11:05:18 -0500

    Update src/qt/walletview.cpp

--- src/qt/walletview.cpp
+++ src/qt/walletview.cpp
@@ -3,7 +3,7 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include "walletview.h"
-#include "privacypage.h"
+#include "privacydialog.h"
 #include "addressbookpage.h"
 #include "askpassphrasedialog.h"
 #include "bip38tooldialog.h"
