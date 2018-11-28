5968c1f1621418376444195b96ce33719ad3bf14
MotoAcidic <travisfinch01@gmail.com>
2018-11-28 13:35:19 -0500

    Update privacydialog.h

--- src/qt/privacydialog.h
+++ src/qt/privacydialog.h
@@ -49,11 +49,11 @@ public:
     void setZCcbcControlLabels(int64_t nAmount, int nQuantity);
 
 public slots:
-    void setBalance( //const CAmount& balance, const CAmount& unconfirmedBalance, const CAmount& immatureBalance,
+    void setBalance( const CAmount& balance, const CAmount& unconfirmedBalance, const CAmount& immatureBalance,
         const CAmount& zerocoinBalance,
         const CAmount& unconfirmedZerocoinBalance,
-        const CAmount& immatureZerocoinBalance);
-                    //const CAmount& watchOnlyBalance, const CAmount& watchUnconfBalance, const CAmount& watchImmatureBalance);
+        const CAmount& immatureZerocoinBalance,
+                    const CAmount& watchOnlyBalance, const CAmount& watchUnconfBalance, const CAmount& watchImmatureBalance);
 protected:
     virtual void keyPressEvent(QKeyEvent* event);
 
