094c4a79ccd13dfc948acdc3ab6c4b6acb37e00c
MotoAcidic <travisfinch01@gmail.com>
2019-01-16 07:42:40 -0500

    Update masternode-payments.cpp

--- src/masternode-payments.cpp
+++ src/masternode-payments.cpp
@@ -225,7 +225,7 @@ bool IsBlockValueValid(const CBlock& block, CAmount nExpectedValue, CAmount nMin
 }
 
 //Current Working 1.1.0.1
-/*
+
 bool IsBlockPayeeValid(const CBlock& block, int nBlockHeight)
 {
     if (!masternodeSync.IsSynced()) { //there is no budget data to use to check anything -- find the longest chain
@@ -317,7 +317,7 @@ bool IsBlockPayeeValid(const CBlock& block, int nBlockHeight)
 					
 	}
 }
-*/
+
 
 // Current Production working code
 /*
@@ -411,7 +411,7 @@ bool IsBlockPayeeValid(const CBlock& block, int nBlockHeight)
     }
 }
 */
-
+/*
 bool IsBlockPayeeValid(const CBlock& block, int nBlockHeight)
 {
     if (!masternodeSync.IsSynced()) { //there is no budget data to use to check anything -- find the longest chain
@@ -554,6 +554,7 @@ bool IsBlockPayeeValid(const CBlock& block, int nBlockHeight)
                 return true;
 	
 }
+*/
 
 
 
