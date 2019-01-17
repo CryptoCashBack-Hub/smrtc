1bb682c0707563b36d207738fb26b3d4da29da4e
MotoAcidic <travisfinch01@gmail.com>
2019-01-17 16:38:36 -0500

    Update init.h

--- src/init.h
+++ src/init.h
@@ -7,7 +7,7 @@
 #define BITCOIN_INIT_H
 
 #include <string>
-
+class CScheduler;
 class CWallet;
 
 namespace boost
@@ -22,7 +22,7 @@ bool ShutdownRequested();
 void Shutdown();
 void PrepareShutdown();
 bool AppInit2(boost::thread_group& threadGroup, CScheduler& scheduler);
-bool AppInit2(boost::thread_group& threadGroup);
+//bool AppInit2(boost::thread_group& threadGroup);
 
 
 /** The help message mode determines what help message to show */
